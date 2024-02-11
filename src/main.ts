c77959a3b03e5b40481ec060ad7fad410a8ff6bc
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2024-02-11 18:00:56 -0300

    add port configuration and default .env values

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(process.env.NESTJS_BASEPATH);
-  await app.listen(3000);
+  app.setGlobalPrefix(process.env.NESTJS_BASEPATH || '/');
+  await app.listen(process.env.NESTJS_PORT || 3000);
 }
 bootstrap();
