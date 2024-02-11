09fbdf34e71d5dd5b8c9427f3e0d8d42ea2c9db6
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2024-02-11 17:23:12 -0300

    add .env.example, install .env dependencies and set api basepath

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AttributeController } from './attribute/attribute.controller';
 import { CharacterController } from './character/character.controller';
 import { CharacterService } from './character/character.service';
 import { AttributeService } from './attribute/attribute.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AttributeController, CharacterController],
   providers: [AttributeService, CharacterService],
 })
